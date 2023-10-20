2b97c4b7869000e99d2854e412f79457fa2c0983
JackoTF <jacko.tinoco@tecsup.edu.pe>
2023-10-20 15:24:31 -0500

    Navigation Page

--- AMM_LAB07_TINOCO/AMM_LAB07_TINOCO/App.xaml.cs
+++ AMM_LAB07_TINOCO/AMM_LAB07_TINOCO/App.xaml.cs
@@ -10,7 +10,7 @@ namespace AMM_LAB07_TINOCO
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
